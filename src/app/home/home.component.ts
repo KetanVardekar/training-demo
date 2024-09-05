82d89373d112d78944a739a0185b8c16b3262854
Ketan Vardekar <ketan@elegantmicroweb.com>
2024-09-05 15:23:08 +0530

    Updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { ContactComponent } from '../contact/contact.component';
-import { myJson } from '../../../public/data';
+
 import { TextColorDirective } from '../Directive/text-color.directive';
 import { InputComponent } from '../input/input.component';
 
@@ -13,13 +13,13 @@ import { InputComponent } from '../input/input.component';
 })
 export class HomeComponent implements OnInit{
   @HostBinding('style.backgroundColor') backgroundColor: string = 'lightblue';
-  jsonData = myJson;
+
   label= 'Email Address';
   placeHolder= 'Enter email here '
   html='<input type="email>"'
   
   ngOnInit(): void {
-    console.log(this.jsonData);
+   
   }
 
 
